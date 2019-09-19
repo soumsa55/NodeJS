04a54dc395052ec55f8d0c1bb90c1af56fdcc956
soumsa55 <39264251+soumsa55@users.noreply.github.com>
2019-09-18 21:41:35 -0700

    Created Node Tutorial JS file
    Node Tutorial Module

---
+++ NodeTutorial.js
@@ -0,0 +1,10 @@
+var Tutor=require('./Tutorial.js');
+exports.NodeTutorial=function()
+{
+console.log("Node Tutorial")
+function pTutor()//this.pTutor = function ()//
+{
+var PTutor=Tutor
+PTutor.tutorial();
+}
+}
