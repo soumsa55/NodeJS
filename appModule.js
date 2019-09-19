7f7d100a4b4cc6a17e91865a4375bf5400da4531
soumsa55 <39264251+soumsa55@users.noreply.github.com>
2019-09-18 21:42:32 -0700

    Application module file
    Committing the module application file

---
+++ appModule.js
@@ -0,0 +1,7 @@
+var localTutor=require('./NodeTutorial.js');
+localTutor.NodeTutorial();
+localTutor.NodeTutorial.pTutor();
+
+//Or use this code
+//var tut = new localTutor.NodeTutorial();  // Create and save object
+//tut.pTutor();  // Call function on object
